4e851590b19304548ba65ac37e20cd3ee98f4465
seongddiyong <kodd1102@naver.com>
2023-08-26 17:20:30 +0900

    first commit

---
+++ src/main/java/com/example/practiceplace/PracticePlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticePlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticePlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practiceplace/PracticePlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticePlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
